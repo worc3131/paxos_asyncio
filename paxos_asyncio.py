01d553f079d603442862bdabb9c0a22007d43a6c
George <web.gwh@gmail.com>
2020-11-09 12:00:33 +0000

    Set up message passing and handling framework

--- paxos_asyncio.py
+++ paxos_asyncio.py
@@ -3,12 +3,14 @@ from __future__ import annotations
 
 from abc import ABC
 import asyncio
+import collections
+import functools
 import random
-import time
-from typing import List, Tuple
+from typing import Deque, List, Optional, Tuple
 
-FUZZ_MAX_TIME = 1
+FUZZ_MAX_TIME = 0.5
 PROB_KILL = 0.01
+PROB_MESSAGE_LOSS = 0.01
 
 async def fuzz() -> None:
     await asyncio.sleep(random.random() * FUZZ_MAX_TIME)
@@ -16,13 +18,19 @@ async def fuzz() -> None:
 def should_chaos_monkey_die():
     return random.random() < PROB_KILL
 
-def log_message(color: str, id: int, msg : str) -> None:
+def should_chaos_monkey_lose_message():
+    return random.random() < PROB_MESSAGE_LOSS
+
+def do_log(color: str, id: int, msg : str) -> None:
     print(color, f'[{id}] {msg}')
+
+def do_system_log(msg):
+    print("\033[0m", f'[SYS] {msg}')
 
 class Coordinator:
     # ANSI colors
     c = (
-        "\033[0m",  # End of color
+        "\033[0m",   # Plain
         "\033[36m",  # Cyan
         "\033[91m",  # Red
         "\033[35m",  # Magenta
@@ -36,8 +44,17 @@ class Coordinator:
         id = len(self.processors)-1
         return id, self.c[id % len(self.c)]
 
-    def get_quorum_of_acceptors(self):
-        acceptors = [x for x in self.processors if isinstance(x, Acceptor)]
+    def send_letter(self, letter: Letter) -> None:
+        if should_chaos_monkey_lose_message():
+            do_system_log('CHAOS MONKEY losing message from'
+                          f' {letter.frm} to {letter.to}')
+        else:
+            self.processors[letter.to].mailbox.append(letter)
+
+    def get_quorum_of_acceptors(self, exclude) -> List[int]:
+        acceptors = [x.id for x in self.processors
+                     if isinstance(x, Acceptor)
+                     and x not in exclude]
         # for now just return all
         return acceptors
 
@@ -59,27 +76,86 @@ class IncProposalGenerator(ProposalGenerator):
         return result
 
 
+class Message(ABC):
+    pass
+
+    def __str__(self):
+        raise NotImplementedError
+
+
+class Letter:
+    def __init__(self, frm: int, to: int, message: Message) -> None:
+        self.frm = frm
+        self.to = to
+        self.message = message
+
+
+class PrepareMessage(Message):
+    def __init__(self, number: int) -> None:
+        self.number = number
+
+    def __str__(self):
+        return 'PREPARE N=' + str(self.number)
+
+
+class PromiseMessage(Message):
+    def __init__(self, number: int,
+                 prev_acc_number: Optional[int],
+                 prev_acc_value: Optional[int]) -> None:
+        self.number = number
+        self.prev_acc_number = prev_acc_number
+        self.prev_acc_value = prev_acc_value
+
+    def __str__(self):
+        return f'PROMISE N={self.number},' \
+               f' PREV_ACC_N={self.prev_acc_number}' \
+               f' PREV_ACC_VAL={self.prev_acc_value}'
+
 class Processor(ABC):
 
     def __init__(self, coordinator: Coordinator) -> None:
         self.coordinator = coordinator
         self.id, self.color = coordinator.register(self)
         self.alive = True
+        self.mailbox: Deque[Message] = collections.deque()
+        self.handle_message = functools.singledispatch(self._handle_message_catch)
         self.log('created as an ' + self.__class__.__name__.lower())
 
+    async def send_message_to(self, other: int, message: Message) -> None:
+        await fuzz()
+        letter = Letter(self.id, other, message)
+        self.coordinator.send_letter(letter)
+        self.log("sent message to " + str(other) + ": " + str(message))
+
+    async def process_mailbox(self):
+        while len(self.mailbox) > 0:
+            letter = self.mailbox.popleft()
+            assert letter.to == self.id
+            await fuzz()
+            await self.handle_message(letter.message, letter.frm)
+            self.log(f"recv message from {letter.frm}: {letter.message}")
+            await fuzz()
+
+    async def _handle_message_catch(self, msg: Message, frm:int) -> None:
+        raise Exception("No message handling logic for message type " +
+                        msg.__class__.__name__ + " in " +
+                        self.__class__.__name__ + " sent by " +
+                        str(frm))
+
     def log(self, msg: str) -> None:
-        log_message(self.color, self.id, msg)
+        do_log(self.color, self.id, msg)
 
     async def run(self) -> None:
         while self.alive:
             if should_chaos_monkey_die():
-                self.log('killed by chaos monkey')
+                self.log('killed by CHAOS MONKEY')
                 self.alive = False
                 continue
+            await self.process_mailbox()
             await fuzz()
-            self.action_loop()
+            await self.action_loop()
 
-    def action_loop(self) -> None:
+    async def action_loop(self) -> None:
         raise NotImplementedError
 
 
@@ -87,8 +163,25 @@ class Acceptor(Processor):
 
     def __init__(self, coordinator: Coordinator) -> None:
         super().__init__(coordinator)
+        self.highest_number_seen: Optional[int] = None
+        self.prev_acc_number: Optional[int] = None
+        self.prev_acc_value: Optional[int] = None
+        self.handle_message.register(PrepareMessage, self._handle_prepare_message)
 
-    def action_loop(self):
+    async def _handle_prepare_message(self, msg: PrepareMessage, frm: int) -> None:
+        number = msg.number
+        if self.highest_number_seen is None\
+                or self.highest_number_seen < number:
+            self.highest_number_seen = number
+            response = PromiseMessage(number,
+                                 self.prev_acc_number,
+                                 self.prev_acc_value)
+            await self.send_message_to(frm, response)
+        else:
+            # TODO optional: send a denial
+            pass
+
+    async def action_loop(self) -> None:
         pass
 
 
@@ -99,10 +192,11 @@ class Proposer(Acceptor):
                  proposal_generator: ProposalGenerator) -> None:
         super().__init__(coordinator)
         self.proposal_generator = proposal_generator
-        self.proposal = None
-        self.number = None
-        self.acceptors = None
+        self.proposal: Optional[int] = None
+        self.number: Optional[int] = None
+        self.acceptors: Optional[List[int]] = None
         self.i_think_im_leader = False
+        self.handle_message.register(PromiseMessage, self._handle_promise_message)
 
     @property
     def i_think_im_leader(self) -> bool:
@@ -112,18 +206,23 @@ class Proposer(Acceptor):
     def i_think_im_leader(self, x: bool) -> None:
         self._i_think_im_leader = x
 
-    def action_loop(self) -> None:
-        super().action_loop()
-        if self.proposal is None:
-            self.generate_proposal()
+    async def _handle_promise_message(self, msg:PromiseMessage, frm:int):
+        pass
 
-    def generate_proposal(self):
+    async def action_loop(self) -> None:
+        await super().action_loop()
+        if self.proposal is None:
+            await self.generate_proposal()
+
+    async def generate_proposal(self):
         p, n = self.proposal_generator.get_proposal()
         if self.number is not None:
             assert n > self.number  # a rule of paxos
         self.proposal = p
         self.number = n
-        self.coordinator.get_quorum_of_acceptors()
+        self.acceptors = self.coordinator.get_quorum_of_acceptors(exclude=[self])
+        for a in self.acceptors:
+            await self.send_message_to(a, PrepareMessage(n))
 
 
 class Learner(Processor):
@@ -131,7 +230,7 @@ class Learner(Processor):
     def __init__(self, coordinater: Coordinator) -> None:
         super().__init__(coordinater)
 
-    def action_loop(self) -> None:
+    async def action_loop(self) -> None:
         pass
 
 
@@ -144,7 +243,6 @@ async def run_paxos() -> None:
         processors.append(Proposer(coord, mess_gen))
     for i in range(3):
         processors.append(Acceptor(coord))
-    tasks = [loop.create_task(p.run()) for p in processors]
-    for task in tasks:
-        await task
-    print('Done!')
+    tasks = [p.run() for p in processors]
+    await asyncio.gather(*tasks)
+    do_system_log('Done!')
